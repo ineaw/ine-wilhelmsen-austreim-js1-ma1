44a217558453e9dcd070cddb93a31b3d4777c5ca
ineaw <ineaw@users.noreply.github.com>
2021-01-24 20:20:17 +0100

    removed excessive question text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 //Question 1:
-//Create an object called cat.
-//Give the object one property called complain. complain's value should be a method (a function) which logs the string "Meow!".
-
 
 // Answer
 
@@ -14,8 +11,6 @@ cat.complain();
 
 
 // Question 2:
-// Select the h3 from the HTML using the querySelector method and assign it to a variable called heading.
-//Change its innerHTML value to "Updated heading".
 
 // Answer
 
@@ -24,7 +19,6 @@ heading.innerHTML = "Updated Heading";
 
 
 // Question 3:
-// Use the style property on the heading variable from the question above to change its font size to "2em".
 
 // Answer
 
@@ -32,7 +26,6 @@ heading.style.fontSize = "2em";
 
 
 // Question 4:
-// Add a class to the heading variable called subheading.
 
 // Answer
 
@@ -40,8 +33,6 @@ heading.style.fontSize = "2em";
 heading.classList.add(".subheading");
 
 // Question 5:
-// Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
-// Loop through the p elements and change the colour of each to "red".
 
 // Answer
 
@@ -52,9 +43,6 @@ for(let i = 0; i < paragraphs.length; i++) {
 }
 
 // Question 6:
-// Select the div with a class of results, 
-// assign it to a variable called resultsContainer and set its inner HTML 
-// to be <p>New paragraph</p> and its background colour to be yellow.
 
 
 
@@ -69,9 +57,6 @@ resultsContainer.style.backgroundColor = "yellow";
 
 
 // Question 7:
-// Create a function that has one parameter called list.
-// Inside the function, loop through the list parameter and console log the name property in each object.
-// Call the function and pass in the cats variable in the script.js file in the repo.
 
 const cats = [
     {
@@ -98,14 +83,6 @@ function catList(list) {
 catList(cats);
 
 // Question 8:
-// Create a function called createCats. The function will have one parameter called cats.
-// Inside the function loop through the value passed in as cats and create HTML for each object in the array.
-// Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
-// If the age property is missing, it should display “Age unknown” instead.
-// Return the HTML from the function.
-// Call the function and pass in the cats array as the argument.
-// Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.
-
 
 // Answer
 
